f367db9d15ea836def4d142ba9fbd05ba7b5013d
Ekaterina Soboleva <ek.a.soboleva@ya.ru>
2024-12-07 12:46:18 +0300

    +upd2

--- test_cart_noautorize.py
+++ test_cart_noautorize.py
@@ -9,41 +9,30 @@ url = "https://demowebshop.tricentis.com"
 
 # регистрируемся через API
 def test_login_successful():
-    with allure.step("Login"):
-        response = requests.post(
-            url=url + "/login",
-            data={"Email": login_test, "Password": password_test, "RememberMe": False},
-            allow_redirects=False
-        )
-    with allure.step("Verify successful authorization"):
-        assert response.status_code == 302
+    with allure.step("Open Page"):
+        response = requests.get(url + "/login")
 
     # получаем token в куках
-    with allure.step("Get token"):
-        token = response.cookies.get("NOPCOMMERCE.AUTH")
+    with allure.step("Get cookie"):
+        notoken = response.cookies.get("Nop.customer")
 
     # добавляем товары в корзину через API
     with allure.step("Add to shopping cart"):
-        response = requests.post(url=url + "/addproducttocart/catalog/31/1/1", cookies={"NOPCOMMERCE.AUTH": token})
-        assert response.status_code == 200
-
-        response = requests.post(url=url + "/addproducttocart/catalog/52/1/1", cookies={"NOPCOMMERCE.AUTH": token})
+        response = requests.post(url=url + "/addproducttocart/catalog/31/1/1", cookies={"Nop.customer": notoken})
         assert response.status_code == 200
 
     # передаем куки
     with allure.step("Send cookie"):
         browser.open("https://demowebshop.tricentis.com/cart")
-        browser.driver.add_cookie({"name": "NOPCOMMERCE.AUTH", "value": token})
+        browser.driver.add_cookie({"name": "Nop.customer", "value": notoken})
         browser.driver.refresh()
 
     # проверяем, что товар в корзине
     with allure.step("Check shopping cart"):
-        browser.all(".product-name").should(have.exact_texts("14.1-inch Laptop", "Music 2"))
+        browser.element(".product-name").should(have.text("14.1-inch Laptop"))
 
     # чистим корзину
     with allure.step("Check shopping cart"):
-        browser.element(".remove-from-cart").click()
-        browser.element(by.xpath("//input[@name='updatecart']")).click()
         browser.element(".remove-from-cart").click()
         browser.element(by.xpath("//input[@name='updatecart']")).click()
         browser.element(".page-body").should(have.text("Your Shopping Cart is empty!"))
